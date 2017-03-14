e83df661ebe81c3a947360a5da6b020e619c5700
Konrad Kale <konrad.kale@icloud.com>
2017-03-14 14:35:57 -0400

    updated

--- wp-content/themes/kaletheme/template-parts/about-template.php
+++ wp-content/themes/kaletheme/template-parts/about-template.php
@@ -11,32 +11,31 @@
 <div class="abt-cont">
   <img class="alignnone size-full wp-image-184" src="/wp-content/uploads/2017/02/headshot.png" alt="" width="220" height="220" />
   <div class="abt-desc">
-    Yo I'm Konrad, I'm analyst / developer based in Philly with over 5 years of experience
-    in various industries including Fintech, Investment Management, Telecommunication,
-     Insurance and Nonprofit.
-  I develop responsive and beautiful websites for small businesses and entrepernuers.
+    Yo I'm Konrad, I'm an analyst / developer based in Philly. I have over 5 years of experience
+    developing websites and analyzing data for businesses and entreprenuers in various industries including Fintech, Investment Management, Telecommunication,
+     Insurance and Non-profits .
   <br />
   My greatest joy is helping my clients achieve their goals. Throughout my career,
-   my role has always been centered around the client. How do I make my clients'
-   lives, experience, and relationship with the company better? It is such a cool
+   my role has always been centered around the client. It is such a cool
    experience to assist my partners in creating web sites that help
    them drive sales. My goal in development not only help my client drive new sales
     but also present their brand to their target audience and create successful
      long term partnerships.
   <br />
-  My Clients and Colleagues would describe be as having a very good attention
+    &nbsp;&nbsp;&nbsp;&nbsp;   My clients and colleagues would describe be as having a very good attention
   to detail, quick learner and fun to work it :) I love being in new and challenging
   situations, I think it makes me better both as a professional and personal.
   Creativity and passion has been key to my personal success in analytics,
   development and management. <br />
-  Things I always keep in mind when I build my sites: W3C standards, implementation
+    &nbsp;&nbsp;&nbsp;&nbsp; Things I always keep in mind when I build my sites: W3C standards, implementation
   of SEO strategies, capabilities of different web browsers, and cross platform as
   well as marketing strategies and building an organic social media presence. <br  />
  <br  />
-  Technology is changing at an incredible pace, and I keep myself educated on
+    &nbsp;&nbsp;&nbsp;&nbsp; Technology is changing at an incredible pace, and I keep myself educated on
    everything to make sure you won't be left behind. I’m known for implementing
     the latest technologies and staying on top of the latest design trends.
-  If you're looking for a heads-down, micromanaged, do-as-he's-told developer,
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp; If you're looking for a heads-down, micromanaged, do-as-he's-told developer,
   then I am not your guy. If you're looking for a natural leader, who inspires
   confidence, capable of making decisions, improves morale, and finds creative
   and effective solutions to challenges that arise, here I am. But don't take my
