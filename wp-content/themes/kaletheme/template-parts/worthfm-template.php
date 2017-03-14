d9c2b7c490642f95149c4268f51a5df486003f37
Konrad Kale <konrad.kale@icloud.com>
2017-03-14 19:50:36 -0400

    added worthfm page

---
+++ wp-content/themes/kaletheme/template-parts/worthfm-template.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* Template Name: WorthFM Page
+*
+ * @package KaleTheme
+ */
+
+?>
+<?php get_header(); ?>
+
+  <div class="wrth-wrpr">
+    <section>
+      <div id="work-worhfm-section">
+
+            
+
+      </div>
+    </section>
+  </div>
+
+<?php get_footer(); ?>
